a7759526a1efc590499de8e36c5ae3440501f588
JamesXia23 <James_Xia@outlook.com>
2016-07-30 15:48:36 +0800

    创建了第一个测试类

---
+++ spring01/src/com/james/a_hello/App.java
@@ -0,0 +1,31 @@
+package com.james.a_hello;
+
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class App {
+	// 1.通过工厂类的IOC容器创建对象
+	@Test
+	public void testIOC() {
+		// 得到xml资源
+		Resource res = new ClassPathResource(
+				"com/james/a_hello/applicationContext.xml");
+		// 创建ioc容器对象
+		BeanFactory bf = new XmlBeanFactory(res);
+		// 得到容器创建的对象
+		User user = (User) bf.getBean("user");
+		System.out.println(user);
+	}
+	@Test
+	public void testAc() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext(
+				"com/james/a_hello/applicationContext.xml");
+		User user = (User) ac.getBean("user");
+		System.out.println(user);
+	}
+}
